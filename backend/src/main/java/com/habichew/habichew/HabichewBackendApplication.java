8f658f7f1a304ca3ef544f3453fd2bd520051968
MartinNang <53534948+MartinNang@users.noreply.github.com>
2025-06-13 20:52:40 +0100

    set up Spring Boot + Maven backend

---
+++ backend/src/main/java/com/habichew/habichew/HabichewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.habichew.habichew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabichewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabichewBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/habichew/habichew/HabichewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habichew.habichew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabichewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
