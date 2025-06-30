7be4c6a47e76fd95dcd30b6cfcfb2411aaddb7f7
MartinNang <53534948+MartinNang@users.noreply.github.com>
2025-06-30 04:11:39 +0200

    added sml task breakdown to homepage (wip) #18

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,74 +1,92 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import {Image} from 'expo-image';
+import {Button, Platform, StyleSheet, TextInput} from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
+import {HelloWave} from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {ThemedText} from '@/components/ThemedText';
+import {ThemedView} from '@/components/ThemedView';
+import React from 'react';
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">First Change</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    const [habit, onChangeHabit] = React.useState("");
+    const [smlResponse,  setSmlResponse] = React.useState("");
+
+    async function sendHabit() {
+        console.log('Sending Habit...', habit);
+        const url = "http://localhost:8100";
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ habit: "test habit"})
+            })
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const json = await response.json();
+            console.log(json);
+            setSmlResponse(json[0]?.generated_text[1].content);
+            console.log('sml response',  json[0]?.generated_text[1].content);
+        } catch(error) {
+            if (error instanceof Error) {
+                console.log(error.message);
+            }
+        }
+    }
+
+    return (
+        <ParallaxScrollView
+            headerBackgroundColor={{light: '#A1CEDC', dark: '#1D3D47'}}
+            headerImage={
+                <Image
+                    source={require('@/assets/images/partial-react-logo.png')}
+                    style={styles.reactLogo}
+                />
+            }>
+            <ThemedView style={styles.titleContainer}>
+                <ThemedText type="title">Welcome to Habichew</ThemedText>
+            </ThemedView>
+            <ThemedView style={styles.stepContainer}>
+                <ThemedText type="subtitle">Insert your habit!</ThemedText>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeHabit}
+                    value={habit}
+                />
+                <Button onPress={() => sendHabit()} title={'Chew!'}></Button>
+
+            </ThemedView>
+            <ThemedView style={styles.stepContainer}>
+                <ThemedText type="default">Recommended tasks:</ThemedText>
+                <ThemedText type="default">{smlResponse}</ThemedText>
+            </ThemedView>
+        </ParallaxScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        height: 178,
+        width: 290,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
+    input: {
+        backgroundColor: 'rgb(238, 238, 238)',
+        padding: 8,
+        borderRadius: 8,
+    }
 });
--- sml/transformers_tinyllama.py
+++ sml/transformers_tinyllama.py
@@ -1,19 +1,25 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app) # allow CORS for all domains on all routes.
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/", methods = ['POST'])
+@cross_origin()
 def createTaskRecommendations():
+    req_data = request.get_json()
     required_params = ['habit']
-    missing_params = [key for key in required_params if key not in request.args.keys()]
+    print(request.form.keys)
+    missing_params = [key for key in required_params if key not in req_data.keys()]
 
     if len(missing_params)==0:
         #access sml response
         pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
         messages = [
-            {"role": "user", "content": "Break down habit into bulleted list of 5 tasks that can be directly and chronologically executed: %s. No examples." %(request.args['habit'])},
+            {"role": "user", "content": "Break down habit into bulleted list of 5 tasks that can be directly and chronologically executed: %s. No examples." %(req_data['habit'])},
         ]
         return jsonify(pipe(messages, max_length=256))
     else:
@@ -25,5 +31,5 @@ def createTaskRecommendations():
          return jsonify(resp)
 
 if __name__ == '__main__':
-    port = 8081 #the custom port you want
+    port = 8100 #the custom port you want
     app.run(host='0.0.0.0', port=port)
