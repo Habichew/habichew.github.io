6673425d59ab811daf04fc9587db8ab786d1fd03
MartinNang <53534948+MartinNang@users.noreply.github.com>
2025-07-11 15:43:40 +0100

    use camelCase in init.sql to use the same naming convention in the entire backend

--- database/init/init.sql
+++ database/init/init.sql
@@ -12,19 +12,19 @@ CREATE TABLE IF NOT EXISTS users (
   `username` varchar(255),
   `password` varchar(255),
     -- 2083 is the most recommended length for URL
-  `profile_img` varchar(2083) DEFAULT 'https://i.ibb.co/q3MfyBnr/habichew.png',
+  `profileImg` varchar(2083) DEFAULT 'https://i.ibb.co/q3MfyBnr/habichew.png',
   `mood` varchar(255),
-  `pet_id` integer,
+  `petId` integer,
   `score` integer,
-  `fuel_status` double,
-  `current_planet_id` integer,
-  `visited_planets` integer,
-  `finished_planets` integer,
-  `user_habits_id` integer,
-  `tasks_num` integer,
+  `fuelStatus` double,
+  `currentPlanetId` integer,
+  `visitedPlanets` integer,
+  `finishedPlanets` integer,
+  `userHabitsId` integer,
+  `tasksNum` integer,
     -- Record created and updated time automatically
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `createdAt` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- Insert sample users data
@@ -34,8 +34,8 @@ INSERT INTO users (username, email, password) VALUES
 
 CREATE TABLE IF NOT EXISTS user_habits (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `user_id` integer,
-  `habit_id` integer
+  `userId` integer,
+  `habitId` integer
 );
 
 CREATE TABLE IF NOT EXISTS pets (
@@ -45,21 +45,21 @@ CREATE TABLE IF NOT EXISTS pets (
   `personality` varchar(255),
   `level` integer,
   `hunger` integer,
-  `created_at` timestamp
+  `createdAt` timestamp
 );
 
 CREATE TABLE IF NOT EXISTS `planets` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
-  `encounter_id` integer,
-  `postcard_id` integer,
-  `game_id` integer
+  `encounterId` integer,
+  `postcardId` integer,
+  `gameId` integer
 );
 
 CREATE TABLE IF NOT EXISTS `habits` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
-  `task_id` integer
+  `taskId` integer
 );
 
 CREATE TABLE IF NOT EXISTS `tasks` (
@@ -70,39 +70,39 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   `level` integer,
   `priority` integer,
   `recommendation` varchar(255),
-  `category_id` integer,
-  `habit_id` integer,
-  `due_at` datetime,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `categoryId` integer,
+  `habitId` integer,
+  `dueAt` datetime,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
     );
 
 -- Insert sample tasks data
 INSERT INTO tasks (description, score, level, priority, recommendation, category_id, due_at) VALUES ('eat healthily', '50', '3', 2, '', 1, '2026-01-01'), ('study for exam', '30', '3', 1, '', 1, '2025-07-10');
 
-CREATE TABLE IF NOT EXISTS `habit_categories` (
+CREATE TABLE IF NOT EXISTS `habitCategories` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255)
 );
 
 -- Insert sample habit_categories data
-INSERT INTO habit_categories (name) VALUES
+INSERT INTO habitCategories (name) VALUES
 ('Health'),
 ('Studies'),
 ('Hobbies'),
 ('Mindfulness');
 
-ALTER TABLE `users` ADD FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (`petId`) REFERENCES `pets` (`id`);
 
-ALTER TABLE `users` ADD FOREIGN KEY (`current_planet_id`) REFERENCES `planets` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (`currentPlanetId`) REFERENCES `planets` (`id`);
 
-ALTER TABLE `users` ADD FOREIGN KEY (`user_habits_id`) REFERENCES `user_habits` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (`userHabitsId`) REFERENCES `user_habits` (`id`);
 
-ALTER TABLE `user_habits` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `userHabits` ADD FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
 
-ALTER TABLE `user_habits` ADD FOREIGN KEY (`habit_id`) REFERENCES `habits` (`id`);
+ALTER TABLE `userHabits` ADD FOREIGN KEY (`habitId`) REFERENCES `habits` (`id`);
 
-ALTER TABLE `habits` ADD FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`);
+ALTER TABLE `habits` ADD FOREIGN KEY (`taskId`) REFERENCES `tasks` (`id`);
 
-ALTER TABLE `tasks` ADD FOREIGN KEY (`category_id`) REFERENCES `habit_categories` (`id`);
+ALTER TABLE `tasks` ADD FOREIGN KEY (`categoryId`) REFERENCES `habit_categories` (`id`);
 
 -- Add your existing database schema here
